e8525aacc114129df8d8b24d400a3970946a4b23
brewchetta <you@example.com>
2019-01-12 11:52:17 -0500

    Initialized animation files

---
+++ src/CardComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Card from './Animations/Card'
+
+export default class CardComponent extends React.Component {
+
+  render() {
+    return (
+      <div className='card-component'>
+      </div>
+    )
+  }
+}
