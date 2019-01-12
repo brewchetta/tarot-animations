40c9571cc96928f5bb302d7fce2890bee2ee4dda
brewchetta <you@example.com>
2019-01-12 13:20:49 -0500

    Added moon assets

---
+++ src/Animations/17_Star.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StarAnimation = props => {
+
+
+  return (
+    <div className='card-animation'>
+
+    </div>
+  )
+}
+
+export default StarAnimation
--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
-// import Card from './Animations/Card'
+import Card from './Animations/17_Star'
 
 export default class CardComponent extends React.Component {
 
   render() {
     return (
       <div className='card-component'>
+        <Card />
       </div>
     )
   }
