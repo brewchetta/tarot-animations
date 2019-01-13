0ddb57e247bfe4b4a0a1700a9bb46e73d66fa56c
brewchetta <you@example.com>
2019-01-13 12:04:22 -0500

    Star static and some dynamic elements

--- src/Animations/17_Star.js
+++ src/Animations/17_Star.js
@@ -2,12 +2,24 @@
 // Lighthouse center glows luminousl (possibly use firefly from moon)
 
 import React from 'react'
+import lighthouse from '../Assets/17_Star_Lighthouse.png'
+import stars from '../Assets/17_Star_Northstars.png'
 
 const StarAnimation = props => {
 
 
   return (
     <div className='card-animation'>
+
+      <img alt='' src={lighthouse}
+      className='card-fit-width'
+      style={{ filter: 'hue-rotate(90deg)', top: '20%', zIndex: '100' }}
+      />
+
+      <img alt='' src={stars}
+      className='rotate'
+      style={{ height: '150%', position: 'absolute', left: '-50%', top: '-35%' }}
+      />
 
     </div>
   )
--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from './Animations/16_Tower'
+import Card from './Animations/17_Star'
 
 export default class CardComponent extends React.Component {
 
